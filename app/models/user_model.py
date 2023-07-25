79e72032fa1492c805732c64470b400ad6d4b8db
aayushdip <138438333+aayushdip@users.noreply.github.com>
2023-07-25 09:30:16 +0545

    Email (#4)
    * feat(datanase and hashing): Post, Get request added with postgres database connection
    
    * feat(email): Added email sending get request
    
    * docs(pylint): Added changes related to pylint
    
    * feat: Added structure to project
    
    * Feat: added feature

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,13 +1,12 @@
 from sqlalchemy import Boolean, Column, Integer, String
-from database import Base
+from app.database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "usersemail"
 
     id = Column(Integer, primary_key=True, index=True)
-    fullname = Column(String,nullable=False)
+    fullname = Column(String, nullable=False)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-
